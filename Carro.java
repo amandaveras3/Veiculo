cd08398fa43ab867b5ee0b864512534d719710fe
Amanda <felixverasamanda@gmail.com>
2025-09-27 23:47:12 -0300

    Create Carro.java

---
+++ Carro.java
@@ -0,0 +1,7 @@
+public class Carro extends Veiculo {
+    private int numeroDePortas;
+
+    public Carro(String marca, String modelo, int ano, int numeroDePortas) {
+        super(marca, modelo, ano);
+        this.numeroDePortas = numeroDePortas;
+    }
